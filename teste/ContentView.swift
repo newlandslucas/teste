3d2d8fcceb3684b0c6ac10daa468af06c80b9679
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-07-14 15:40:41 -0300

    fix: text adjustments from Open finance to Open Finance

---
+++ teste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  teste
+//
+//  Created by Lucas Newlands on 14/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by Lucas Newlands on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
